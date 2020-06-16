f820f8da648f93bf4029d5b2495511e551062f72
Dawid Dziad <dawid.dziad@microsoft.wsei.edu.pl>
2020-06-16 23:32:39 +0200

    Stworzenie prostego menu

--- KółkoKrzyżyk/Interfejs/Class1.cs
+++ KółkoKrzyżyk/Interfejs/Class1.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using Logika;
 
-namespace Interfejs
+namespace Logika
 {
-    public class Class1
+     public static class Class1
     {
-    }
+        public static void MojeMenu()
+        {
+            Console.Title = "Menu";
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\t Witam w grze!");
+                Console.WriteLine("\tKółko i krzyżyk");
+                Console.ResetColor();
+                Console.WriteLine("-------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("(Naciśnij (1-3) aby rozpocząć działania)");
+                Console.ResetColor();
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("\t1-Rozpocznij grę");
+                Console.WriteLine("\t2-Zasady gry");
+                Console.WriteLine("\t3-Zakończ");
+
+                ConsoleKeyInfo klawisz = Console.ReadKey();
+                switch (klawisz.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        WBudowie();
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        WBudowie();
+                        break;
+                    case ConsoleKey.Escape:
+                    case ConsoleKey.D3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+        static void WBudowie()
+        {
+            Console.Write("puste");
+            Console.ReadKey();
+        }
+    }    
 }
+
--- KółkoKrzyżyk/Logika/Program.cs
+++ KółkoKrzyżyk/Logika/Program.cs
@@ -4,9 +4,9 @@ namespace Logika
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Class1.MojeMenu();
         }
     }
 }
